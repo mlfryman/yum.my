b2316a514045750a22b69db68b130fc8cef0e096
Melanie Fryman <melanie@frymanet.com>
2014-09-15 10:08:57 -0500

    added register form

---
+++ client/views/register/register.js
@@ -0,0 +1,12 @@
+(function(){
+  'use strict';
+
+  angular.module('yum.my')
+  .controller('RegisterCtrl', ['$scope', function($scope){
+    $scope.user = {};
+
+    $scope.register = function(){
+      $scope.user = {};
+    };
+  }]);
+})();
